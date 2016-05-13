6f3901f10c682f7afeb3dc562f3640a6df7813ca
Thomas Wienecke <wienecke.t@gmail.com>
2016-05-14 01:24:25 +0200

    Enable SQLite PRAGMA foreign_keys.

--- src/Kis/SqlBackend.hs
+++ src/Kis/SqlBackend.hs
@@ -14,8 +14,7 @@ import Control.Monad.IO.Class
 import Control.Monad.Trans.Reader
 import Database.Persist.Sql as S
 import Database.Persist.Sqlite
-import Database.Sqlite hiding (config)
-import Data.Maybe
+import Database.Sqlite as Sqlite hiding (config)
 import qualified Database.Esqueleto as E
 
 import Kis.Model
@@ -34,7 +33,8 @@ handleKisRequest backend req = runSqlConn (runAction req) backend
 
 inMemoryBackend :: IO SqlBackend
 inMemoryBackend = do
-    connection <- open ":memory:"
+    connection <- Sqlite.open ":memory:"
+    void $ Sqlite.step =<< Sqlite.prepare connection "PRAGMA foreign_keys = ON;"
     backend <- wrapConnection connection (\_ _ _ _ -> return ())
     void $ runSqlConn (runMigrationSilent migrateAll) backend
     return backend
@@ -44,16 +44,7 @@ runAction (CreateBed name) = S.insert (Bed name)
 runAction (CreatePatient patient) = S.insert patient
 runAction (GetPatient pid) = S.get pid
 runAction GetPatients = getPatients
-runAction (PlacePatient patId bedId) =
-    ifM (exists patId &&^ exists bedId) -- Not thread safe.
-        (S.insertUnique $ PatientBed patId bedId)
-        (return Nothing)
-
-exists ::
-    (PersistEntity val, MonadIO m,
-      PersistStore (PersistEntityBackend val)) =>
-     Key val -> ReaderT (PersistEntityBackend val) m Bool
-exists key = liftM isJust (S.get key)
+runAction (PlacePatient patId bedId) = S.insertUnique $ PatientBed patId bedId
 
 getPatients :: MonadIO m => ReaderT SqlBackend m [Entity Patient]
 getPatients = E.select $ E.from $ \p -> return p
